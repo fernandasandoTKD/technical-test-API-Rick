d2eb84f9bc51bbd15cc5ca3eb9ee8433e92ddc1b
Fernanda Sandoval <luisa.sandoval@commity.co>
2025-06-22 21:31:52 -0500

    Annex favorite character

--- test-API/src/app/features/characters/characters.component.ts
+++ test-API/src/app/features/characters/characters.component.ts
@@ -11,6 +11,8 @@ import { CharacterService } from '../../core/services/character.service';
 import { debounceTime } from 'rxjs';
 import { MatButton } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-characters',
@@ -24,22 +26,35 @@ import { MatIconModule } from '@angular/material/icon';
     MatInputModule,
     MatSelectModule,
     MatButton,
-    MatIconModule
+    MatIconModule,
+    MatCardModule,
+    MatSnackBarModule
   ],
   templateUrl: './characters.component.html',
   styleUrls: ['./characters.component.css']
 })
 export class CharactersComponent implements OnInit {
   private characterService = inject(CharacterService);
+  private snackBar = inject(MatSnackBar);
+
 
   // Filtros
   nameControl = new FormControl('');
   statusControl = new FormControl('');
   statuses = ['alive', 'dead', 'unknown'];
 
+
   // Tabla
   dataSource = new MatTableDataSource<any>();
   displayedColumns = ['name', 'status', 'species', 'type', 'gender', 'created', 'actions'];
+
+  //Totales
+  totalBySpecies: { species: string; count: number }[] = [];
+  totalByType: { type: string; count: number }[] = [];
+
+  favoriteCharacter: any = null;
+
+
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
@@ -58,15 +73,55 @@ export class CharactersComponent implements OnInit {
       next: (allCharacters) => {
         this.dataSource.data = allCharacters;
         this.dataSource.paginator = this.paginator;
+        this.calculateTotals(allCharacters);
       },
       error: () => {
         this.dataSource.data = [];
       }
+
     });
+
   }
 
   clearFilters(): void {
     this.nameControl.setValue('');
     this.statusControl.setValue('');
   }
+
+   calculateTotals(characters: any[]): void {
+    const speciesMap = new Map<string, number>();
+    const typeMap = new Map<string, number>();
+
+    for (const char of characters) {
+      const species = char.species || 'Sin especie';
+      const type = char.type || 'Sin tipo';
+
+      speciesMap.set(species, (speciesMap.get(species) || 0) + 1);
+      typeMap.set(type, (typeMap.get(type) || 0) + 1);
+    }
+
+    this.totalBySpecies = Array.from(speciesMap.entries()).map(([species, count]) => ({ species, count }));
+    this.totalByType = Array.from(typeMap.entries()).map(([type, count]) => ({ type, count }));
+  }
+
+setFavorite(character: any): void {
+  if (this.favoriteCharacter && this.favoriteCharacter.id === character.id) {
+    this.favoriteCharacter = null;
+    this.snackBar.open('Favorito eliminado', 'Cerrar', { duration: 3000 });
+  } else {
+    this.favoriteCharacter = character;
+    this.snackBar.open('Personaje marcado como favorito', 'Cerrar', { duration: 3000 });
+  }
+}
+
+showFavoriteDetails(): void {
+  alert(`
+    Nombre: ${this.favoriteCharacter.name}
+    Status: ${this.favoriteCharacter.status}
+    Especie: ${this.favoriteCharacter.species}
+    Género: ${this.favoriteCharacter.gender}
+  `);
+}
+
+
 }
